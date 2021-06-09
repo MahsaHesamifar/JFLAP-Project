43cf4f040538048970e2168b07d59553ff8595f4
MahsaHesamifar <m.hesamifar@yahoo.com>
2021-06-09 03:55:07 +0330

    adding team info + styles

--- src/Components/Team/index.jsx
+++ src/Components/Team/index.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 
 const Team = () => {
-  return (<div className="team-container">
-    About us
-  </div>);
+  return (
+    <div className="team-container">
+      <h1>Team Members:</h1>
+      <ul>
+        <li className="orange">
+          <span>Fatemeh</span> Safari
+        </li>
+        <li className="purple">
+          <span>Shaghayegh</span> Naghizadeh
+        </li>
+        <li className="blue">
+          <span>Sogol</span> Shishechiha
+        </li>
+        <li className="orange">
+          <span>Atena</span> Bagheri
+        </li>
+        <li className="purple">
+          <span>Hiva</span> Bahrami
+        </li>
+        <li className="blue">
+          <span>Mahsa</span> Hesamifar
+        </li>
+      </ul>
+    </div>
+  );
 };
 export default Team;
